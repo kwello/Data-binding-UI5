b03908420944cce1f9ad4b73d1fb067d3cb1d628
Praveen Mishra <praveenmishracool123@gmail.com>
2023-03-07 12:50:26 +0530

    Create a model

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,21 @@
 sap.ui.require([
-    "sap/m/Text"
-],function (Text){
+    "sap/m/Text",
+    "sap/ui/model/json/JSONModel"
+],function (Text, JSONModel){
     "use strict"
 
     //attach an anonymous fucntion to the SAPUI5 'init' event
     sap.ui.getCore().attachInit(function (){
+
+        //create a JSON model from an object literal
+        var oModel = new JSONModel({
+            greetingText: "Hi. my name is ...Tashu"
+        });
+
+        //Assign the model object to the SAPUI5 core
+        sap.ui.getCore().setModel(oModel);
+        //this.getView().setModel(oModel)
+
         //create a text ui element that displays a hardcoded text script
         new Text({
             text: "Hi. my name is ...Praveen"
